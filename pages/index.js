a8cf54570f0c7e7a23257a54beec2627a9b49fc9
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-11-24 20:49:44 -0300

    ajustar frase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-function Home(){
-    return <>
-        <h1>Você é especial!</h1>
-        <p>Fiz esta página no curso de programação apenas para dizer isso!</p>
-        <span>2023 ❤️ @khyquer</span>
-    </>
+function Home() {
+	return (
+		<>
+			<h1>Você é especial para mim</h1>
+			<p>Fiz esta página no curso de programação apenas para dizer isso!</p>
+			<span>2023 ❤️ @khyquer</span>
+		</>
+	)
 }
 
-export default Home;
+export default Home
