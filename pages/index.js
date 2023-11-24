aed0a160ec1c3cb472171b9d61f7ae68bdc72361
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-11-24 02:49:22 +0000

    Adicionar página page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <>
+        <h1>Você é especial!</h1>
+        <p>Fiz esta página no curso de programação apenas para dizer isso!</p>
+        <span>@khyquer</span>
+    </>
+}
+
+export default Home;
