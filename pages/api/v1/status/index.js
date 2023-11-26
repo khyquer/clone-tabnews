849ce401f87a065fe9012103eff3d23d192b101c
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-11-26 12:23:42 -0300

    change index.js from status page to return SQL results

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,8 @@ import database from "../../../../infra/database.js";
 
 async function status(request, response) {
   try {
-    const result = await database.query("SELECT 1 + 1");
-    response.status(200).json({ status: result.result });
+    const result = await database.query("SELECT 1 + 1 as result");
+    response.status(200).json({ status: result.rows[0] });
   } catch (error) {
     response.status(500).json({ status: error.message });
   }
