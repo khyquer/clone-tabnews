e6132419f1bd327791daa3399f50d4778bd81a5c
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-11-26 11:34:47 -0300

    #6 using `database.js` in status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,12 @@
-function status(request, response) {
-  response.status(200).json({ status: 200 });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  try {
+    const result = await database.query("SELECT 1 + 1");
+    response.status(200).json({ status: result.result });
+  } catch (error) {
+    response.status(500).json({ status: error.message });
+  }
 }
 
 export default status;
