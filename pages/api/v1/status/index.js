2801d47ca4a51cfae890f3d151ec0bee9916e6a3
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-11-30 20:36:44 -0300

    #6 Add properties updated_at, version, max_connections and count_connections at `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,27 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   try {
-    const result = await database.query("SELECT 1 + 1 as result");
-    response.status(200).json({ status: result.rows[0] });
+    const updatedAt = new Date().toISOString();
+
+    const resultQueryVersionDataBase =
+      await database.query("SELECT version();");
+
+    const resultMaxConnectionsDataBase = await database.query(
+      "SHOW max_connections;",
+    );
+    const resultCountConnectionsDataBase = await database.query(
+      "SELECT count(*) as count_connections FROM pg_stat_activity;",
+    );
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        version: resultQueryVersionDataBase.rows[0].version,
+        max_connections: resultMaxConnectionsDataBase.rows[0].max_connections,
+        count_connections:
+          resultCountConnectionsDataBase.rows[0].count_connections,
+      },
+    });
   } catch (error) {
     response.status(500).json({ status: error.message });
   }
--- tests/integration/api/v1/status/gets.test.js
+++ tests/integration/api/v1/status/gets.test.js
@@ -3,3 +3,39 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(response.status).toBe(200);
 });
+
+test("GET to /api/v1/status should return property update_at", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+});
+
+test("GET to /api/v1/status should return property update_at is a date", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET to /api/v1/status should return property database", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+
+  expect(responseBody.database).toBeDefined();
+});
+
+test("GET to /api/v1/status should return property database e version, max_connections and connectionsUseds ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+
+  expect(responseBody.database.version).toBeDefined();
+  expect(responseBody.database.max_connections).toBeDefined();
+  expect(responseBody.database.count_connections).toBeDefined();
+});
