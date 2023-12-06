5efce96623d774ce50842e5141bbb7e9c2c79d7a
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-12-05 23:45:00 -0300

    #6 Refactor: Switch from 'Client' to 'Pool' for improved connection handling.

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,26 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const client = new Pool({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    max: 500,
+    connectionTimeoutMillis: 25000,
+    idleTimeoutMillis: 2000,
+    allowExitOnIdle: true,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+
+    return result;
+  } finally {
+    //await client.end();
+  }
 }
 
 export default {
