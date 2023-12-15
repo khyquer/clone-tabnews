92468cc438a9e55170f4c471e055c83e79fb5107
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2023-12-14 21:02:47 -0300

    #6 inprove `index.js` from status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,28 +4,32 @@ async function status(request, response) {
   try {
     const updatedAt = new Date().toISOString();
 
-    const resultQueryVersionDataBase = await database.query(
-      "SHOW server_version",
+    const queryVersionDataBaseResult = await database.query(
+      "SHOW server_version;",
     );
+    queryVersionDataBaseValue = parseInt(queryVersionDataBaseResult.rows[0]);
 
-    const resultMaxConnectionsDataBase = await database.query(
+    const maxConnectionsDataBaseResult = await database.query(
       "SHOW max_connections;",
     );
-    const resultCountConnectionsDataBase = await database.query(
+    maxConnectionsDataBaseValue = parseInt(
+      maxConnectionsDataBaseResult.rows[0],
+    );
+
+    const rountConnectionsDataBaseResult = await database.query(
       "SELECT count(*) as count_connections FROM pg_stat_activity;",
+    );
+    rountConnectionsDataBaseValue = parseInt(
+      rountConnectionsDataBaseResult.rows[0],
     );
 
     response.status(200).json({
       updated_at: updatedAt,
       dependencies: {
         database: {
-          version: resultQueryVersionDataBase.rows[0].server_version,
-          max_connections: new Number(
-            resultMaxConnectionsDataBase.rows[0].max_connections,
-          ),
-          count_connections: new Number(
-            resultCountConnectionsDataBase.rows[0].count_connections,
-          ),
+          version: queryVersionDataBaseValue,
+          max_connections: maxConnectionsDataBaseValue,
+          count_connections: rountConnectionsDataBaseValue,
         },
       },
     });
