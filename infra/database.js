cd6739084bee63aeec1192e743d29a3d4177cf61
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2024-05-02 23:35:27 -0300

    Melhorar DB

--- infra/database.js
+++ infra/database.js
@@ -15,8 +15,8 @@ async function query(queryObject) {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    await client.end();
+  } catch (error) {
+    console.error(error);
   } finally {
     await client.end();
   }
